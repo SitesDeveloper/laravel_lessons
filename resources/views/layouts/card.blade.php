fca648b384426340725c22166ba54125a4bc2ac0
star <sergrom12@yandex.ru>
2021-12-21 21:20:44 +0400

    lesson 13 part2: storage

--- resources/views/layouts/card.blade.php
+++ resources/views/layouts/card.blade.php
@@ -1,6 +1,6 @@
 <div class="col-sm-6 col-md-4">
     <div class="thumbnail">
-        <img src="http://laravel-diplom-1.rdavydov.ru/storage/products/htc_one_s.png" alt="{{$product->name}}">
+        <img src="{{Storage::url($product->image)}}" alt="{{$product->name}}">
         <div class="caption">
             <h3>{{$product->name}}</h3>
             <p>{{$product->price}} руб.</p>
