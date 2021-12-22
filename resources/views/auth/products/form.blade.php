b946d499775a64d53f154b8d036c34ce30466254
star <sergrom12@yandex.ru>
2021-12-22 12:20:51 +0400

    lesson 14. Валидация, FormRequest

--- resources/views/auth/products/form.blade.php
+++ resources/views/auth/products/form.blade.php
@@ -28,22 +28,31 @@
                 <div class="input-group row">
                     <label for="code" class="col-sm-2 col-form-label">Код: </label>
                     <div class="col-sm-6">
+                        @error('code')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
                         <input type="text" class="form-control" name="code" id="code"
-                               value="@isset($product){{ $product->code }}@endisset">
+                               value="{{old("code", isset($product) ? $product->code : null)}}">
                     </div>
                 </div>
                 <br>
                 <div class="input-group row">
                     <label for="name" class="col-sm-2 col-form-label">Название: </label>
                     <div class="col-sm-6">
+                        @error('name')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
                         <input type="text" class="form-control" name="name" id="name"
-                               value="@isset($product){{ $product->name }}@endisset">
+                               value="{{old("name", isset($product) ? $product->name : null)}}">
                     </div>
                 </div>
                 <br>
                 <div class="input-group row">
                     <label for="category_id" class="col-sm-2 col-form-label">Категория: </label>
                     <div class="col-sm-6">
+                        @error('category_id')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
                         <select name="category_id" id="category_id" class="form-control">
                             @foreach ($categories as $category)
                                 <option value="{{$category->id}}"
@@ -61,8 +70,11 @@
                 <div class="input-group row">
                     <label for="description" class="col-sm-2 col-form-label">Описание: </label>
                     <div class="col-sm-6">
-								<textarea name="description" id="description" cols="72"
-                                          rows="7">@isset($product){{ $product->description }}@endisset</textarea>
+                        @error('description')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+                        <textarea name="description" id="description" cols="72"
+                                    rows="7">{{old("description", isset($product) ? $product->description : null)}}</textarea>
                     </div>
                 </div>
                 <br>
@@ -77,9 +89,12 @@
                 <br>
                 <div class="input-group row">
                     <label for="price" class="col-sm-2 col-form-label">Цена: </label>
-                    <div class="col-sm-2">
+                    <div class="col-sm-6">
+                        @error('price')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
                         <input type="text" class="form-control" name="price" id="price"
-                               value="@isset($product){{ $product->price }}@endisset">
+                               value="{{old("price", isset($product) ? $product->price : null)}}">
                     </div>
                 </div>
                 <button class="btn btn-success">Сохранить</button>
