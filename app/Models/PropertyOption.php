00a0ce84a5ae8f93015bbe901cac92fe3c37923e
star <sergrom12@yandex.ru>
2022-07-12 08:39:30 +0400

    Lesson 32: Товарные предложения. С чего начать?

---
+++ app/Models/PropertyOption.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Traits\Translatable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class PropertyOption extends Model
+{
+    use HasFactory, SoftDeletes, Translatable;
+
+    protected $fillable = ['property_id', 'name', 'name_en'];
+
+    public function property()
+    {
+        return $this->belongsTo(Property::class);
+    }
+
+    //TODO: check table name and fields
+    public function skus()
+    {
+        return $this->belongsToMany(Sku::class);
+    }
+}
